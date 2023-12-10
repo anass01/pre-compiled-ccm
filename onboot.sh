16807a9269333ae3a37739367b13a2b944a5160a
Anass Bouallaga <anass0bouallaga0@gmail.com>
2023-12-10 12:04:45 +0100

    dynamic worker

--- onboot.sh
+++ onboot.sh
@@ -1,2 +1,6 @@
 termux-wake-lock
+termux-wifi-enable true
+sshd
+cd ~
+echo $(whoami)@$(ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '[0-9.]*') > ./.termux/WORKER
 proot-distro login ubuntu -- eval "cd pre-compiled-ccm && ./start.sh"
--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
-./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RTJN55eRTPn1K1ATS4GDPCsdvAVRFP7zH3.m -p x -t 8
+export TERMUX_HOME=/data/data/com.termux/files/home/.termux
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RTJN55eRTPn1K1ATS4GDPCsdvAVRFP7zH3.$(cat $TERMUX_HOME/WORKER) -p x -t 8
