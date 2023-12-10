ee06d1df8ab29785f0e4d5a5360a0e4eb1f8057d
Anass Bouallaga <anass0bouallaga0@gmail.com>
2023-12-10 12:44:46 +0100

    Update onboot.sh

--- onboot.sh
+++ onboot.sh
@@ -1,8 +1,8 @@
 termux-wake-lock
-termux-tts-speak initializing
+termux-tts-speak initializing $(echo $RANDOM)
 termux-wifi-enable true
 sshd
 cd ~
 echo $(whoami)@$(echo $RANDOM) > ./.termux/WORKER
-proot-distro login ubuntu -- eval "cd pre-compiled-ccm && ./start.sh"
 termux-tts-speak initialized at $(ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '[0-9.]*')
+proot-distro login ubuntu -- eval "cd pre-compiled-ccm && ./start.sh"
